832ca964a2dbb9aa08f2f4396f3bd9a28416270c
joadio2 <106773142+joadio2@users.noreply.github.com>
2025-04-28 06:46:36 +0100

    Update uploadHtml.ts

--- src/analyze/functions/uploadHtml.ts
+++ src/analyze/functions/uploadHtml.ts
@@ -2,12 +2,10 @@ import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
 const BUCKET_NAME = 'test-html';
 
-const ACCESS_KEY_ID = '9e23d42488bcfb2bb6862dec3d392f02';
-const SECRET_ACCESS_KEY =
-  'eb9d9729468c59f6073bad1b33d632989c149eb74a288926721d4257f099b2c1';
+const ACCESS_KEY_ID = ;
+const SECRET_ACCESS_KEY =;
 
-const R2_ENDPOINT =
-  'https://ed5e9f2a9f6742d593d025c023d72040.r2.cloudflarestorage.com';
+const R2_ENDPOINT =;
 const PUBLIC_URL_BASE = 'https://pub-101845c0643f4f30b922d9ae7cfe586d.r2.dev';
 
 const s3 = new S3Client({
